7df4a76370674aff68ac65f853454ce3afe7d571
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-02-03 12:43:40 +0100

    Init commit

---
+++ src/main/java/net/pistonmaster/pistonclient/PistonClient.java
@@ -0,0 +1,13 @@
+package net.pistonmaster.pistonclient;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class PistonClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
