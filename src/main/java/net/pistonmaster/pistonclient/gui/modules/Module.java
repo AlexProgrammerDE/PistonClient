a6ca99831d89295f24bbda620bac99ae730391b8
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-06-27 08:58:07 +0200

    Add missing module class

---
+++ src/main/java/net/pistonmaster/pistonclient/gui/modules/Module.java
@@ -0,0 +1,33 @@
+package net.pistonmaster.pistonclient.gui.modules;
+
+public abstract class Module {
+    private final String name;
+    private final String description;
+    private boolean activated;
+
+    protected Module(String name, String description) {
+        this(name, description, false);
+    }
+
+    protected Module(String name, String description, boolean activated) {
+        this.name = name;
+        this.description = description;
+        this.activated = activated;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setActivated(boolean activated) {
+        this.activated = activated;
+    }
+
+    public boolean isActivated() {
+        return activated;
+    }
+}
